cfb1595283fd9efb7fd7b5eee6f48b96adf9e31c
Alber <albertorg0087@gmail.com>
2022-11-29 19:03:38 +0000

    basic design OK

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react'
-import './App.css'
+import './assets/App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const nums = [7, 8, 9, 4, 5, 6, 1, 2, 3, 0]
+const operators = ['/', '*', '-', '+', '=']
 
-  return (
-    <div className="App">
+const App = () => {
 
-    </div>
-  )
+	const handleClick = (e) => {
+		const { innerText } = e.target
+		alert(innerText)
+	}
+
+	return (
+		<div className="calculator">
+			<div id="display" className="display">1.234</div>
+
+			<div className="nums-container">
+			<button className="big-h light-gray ac" onClick={handleClick} >AC</button>
+			{nums.map(num => (
+					<button 
+						className={`dark-gray ${num === 0 && 'big-h'}`} 
+						key={num}
+						onClick={handleClick}
+					>
+						{num}
+					</button>
+				))}
+			<button className="light-gray" onClick={handleClick}>.</button>	
+			</div>
+
+			<div className="ops-container">
+				{operators.map(op => (
+					<button 
+						className="orange" 
+						key={op}
+						onClick={handleClick}
+					>
+						{op}
+					</button>
+				))}
+			</div>
+		</div>
+	)
 }
 
 export default App
